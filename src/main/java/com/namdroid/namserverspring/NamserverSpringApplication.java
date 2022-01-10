2dab0c786707e733af3bad818cab1a014a03c3d9
jiseong <njs8590@naver.com>
2022-01-10 21:04:00 +0900

    initial commit

---
+++ src/main/java/com/namdroid/namserverspring/NamserverSpringApplication.java
@@ -0,0 +1,13 @@
+package com.namdroid.namserverspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamserverSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamserverSpringApplication.class, args);
+	}
+
+}
