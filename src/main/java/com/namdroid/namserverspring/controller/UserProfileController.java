792631d4a002aec8c648f8c993ba14c716a33963
jiseong <njs8590@naver.com>
2022-01-31 00:27:31 +0900

    userprofile 기본 CRUD api

---
+++ src/main/java/com/namdroid/namserverspring/controller/UserProfileController.java
@@ -0,0 +1,68 @@
+package com.namdroid.namserverspring.controller;
+
+import com.namdroid.namserverspring.model.UserProfile;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController //Spring 프레임워크는 어노테이션 기반이라서 하위 클래스를 컨트롤러로 인식하고 인스턴스를 생성하게됨.
+public class UserProfileController {
+
+    private Map<String, UserProfile> userProfileMap;
+
+    @PostConstruct
+    public void init(){
+        userProfileMap = new HashMap<>();
+
+        // Test
+        userProfileMap.put("1", new UserProfile("1", "홍길동", "123-1234", "한국"));
+        userProfileMap.put("2", new UserProfile("2", "제니퍼", "234-2345", "영국"));
+
+    }
+
+    /*
+    GET: 데이터 조회
+    POST: 생성
+    PUT: 수정
+    DELETE: 삭제
+     */
+    // Userprofile 을 json 형태로 반환해주는 함수
+    @GetMapping("/user/{id}")
+    public UserProfile getUserProfileById(@PathVariable("id") String id){ // PathVariable 써주면 파라미터를 path에 있는 {id} 로 전달하게됨.
+        return userProfileMap.get(id);
+    }
+
+
+    @GetMapping("/user/all")
+    public List<UserProfile> getUserProfileList(){
+
+        return new ArrayList<UserProfile>(userProfileMap.values());
+    }
+
+    //Userprofile 생성 api
+    @PostMapping("/user/{id}")
+    public void postUserProfile(@RequestParam("id") String id,@RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("address") String address){
+        UserProfile userProfile= new UserProfile(id, name, phone, address);
+        userProfileMap.put(id, userProfile);
+    }
+
+    //Userprofile 수정 api
+    @PutMapping("/user/{id}")
+    public void putUserProfile(@RequestParam("id") String id,@RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("address") String address){
+        UserProfile userProfile = userProfileMap.get(id);
+        userProfile.setName(name);
+        userProfile.setPhone(phone);
+        userProfile.setAddress(address);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public void deleteuserProfile(@RequestParam("id") String id){
+        userProfileMap.remove(id);
+    }
+
+
+}
---
+++ src/main/java/com/namdroid/namserverspring/model/UserProfile.java
@@ -0,0 +1,52 @@
+package com.namdroid.namserverspring.model;
+
+public class UserProfile {
+    private String id;
+    private String name;
+    private String phone;
+    private String address;
+
+    public UserProfile(){}
+
+    // alt+insert : spring tool 소스코드 generator
+    public UserProfile(String id, String name, String phone, String address) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    // getter
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    //setter
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
